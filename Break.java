e27c3d85f805842388117c5b43dfda08c41b5f5a
sri kanya <srikanyakanumuri2004@gmail.com>
2026-04-13 13:59:56 +0530

    first upload

---
+++ Break.java
@@ -0,0 +1,16 @@
+ class 	Main
+{ 	
+	public static void main(String[]args)
+	{
+           int i=1;
+            while(i<10)
+            {
+              System.out.println(i); // 1 2 3 4 5
+              if(i==5)
+              {
+                 break;
+              }
+             i++;
+            }
+         }
+}
---
+++ Break1.java
@@ -0,0 +1,16 @@
+class 	Main
+{ 	
+	public static void main(String[]args)
+	{
+           int i=1;
+            while(i<10)// 1<10 t
+            {
+              if(i==5)
+              {
+                 System.out.println(i);// 5
+                 break;
+              }
+             i++;
+            }
+         }
+}
